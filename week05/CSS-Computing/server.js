a5dc4131c410b859bba9e0c93569fa88806e9707
huangke <huangke0802@163.com>
2020-08-01 00:31:45 +0800

    继续

--- week05/CSS-Computing/server.js
+++ week05/CSS-Computing/server.js
@@ -1,45 +1,5 @@
 const http = require('http');
 
-// http.createServer((requset, response) => {
-//   let body = [];
-//   requset.on('error', (err) => {
-//     console.error(err);
-//   }).on('data', (chunk) => {
-//     body.push(chunk);
-//   }).on('end', () => {
-//     body = Buffer.concat(body).toString();
-//     console.log('body:', body);
-//     response.writeHead(200, {
-//       'Content-Type': 'text/html'
-//     });
-//     response.end(
-//       `<html maaa=a>
-// <head>
-// 			<style>
-// body div #myid{
-// 	width:100px;
-// 	background-color:#ff5000;
-// }
-// body div img{
-// 	width:30px;
-// 	background-color:#ff1111;
-// }
-// 			</style>
-// </head>
-// <body>
-// 	<div>
-// 		<img id="myid" />
-// 		<img />
-// 	</div>
-// </body>
-// </html>
-// `
-//     );
-//   });
-// }).listen(8088)
-
-// console.log('server started')
-
 const server = http.createServer((req, res) => {
   console.log('request received');
   console.log(req.headers);
@@ -65,7 +25,7 @@ body div img{
 <body>
 	<div>
 		<img id="myid" />
-		<img />
+		<img/>
 	</div>
 </body>
 </html>`);
