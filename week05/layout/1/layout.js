cc3a20bb04e77a0eee042ce4c77d904f380b65ca
huangke <huangke0802@163.com>
2020-08-02 23:53:16 +0800

    布局

--- week05/layout/1/layout.js
+++ week05/layout/1/layout.js
@@ -119,6 +119,99 @@ function layout(element) {
     crossSign = 1;
   }
 
+  var crossSpace;
+  if (!style[crossSize]) {
+    crossSpace = 0;
+    elementStyle[crossSIze] = 0;
+    for (var i = 0; i < flexLines.length; i++) {
+      //代码未写全；
+      elementStyle[crossSize] = elementStyle[crossSize] + flex
+    }
+  } else {
+    crossSpace = style[crossSize];
+    for (var i = 0; i < flexLines.length; i++) {
+      crossSpace -= flexLines[i].crossSpace;
+    }
+  }
+
+  if (style.flexWrap === 'wrap-reverse') {
+    crossBase = style[crossSize];
+  } else {
+    crossBase = 0;
+  }
+
+  var lineSize = style[crossSize] / flexLines.length;
+
+  var step;
+  if (style.alignContent === 'flex-start') {
+    crossBase += 0;
+    step = 0;
+  }
+
+  if (style.alignContent === 'flex-end') {
+    crossBase += crossSign * crossSpace;
+    step = 0;
+  }
+
+  if (style.alignContent === 'center') {
+    crossBase += crossSign * crossSpace / 2;
+    step = 0;
+  }
+
+  if (style.alignContent === 'space-between') {
+    crossBase += 0;
+    step = crossSpace / (flexLines.length - 1);
+  }
+
+  if (style.alignContent === 'space-around') {
+    crossBase += crossSign * step / 2;
+    step = crossSpace / (flexLines.length);
+  }
+
+  if (style.alignContent === 'stretch') {
+    crossBase += 0;
+    step = 0;
+  }
+
+  flexLines.forEach(function(items) {
+    var lineCrossSize = style.alignContent === 'stretch' ?
+      items.crossSpace + crossSpace / flexLines.length : items.crossSpace;
+
+    for (var i = 0; i < items.length; i++) {
+      var item = items[i];
+      var itemStyle = getStyle[item];
+      var align = itemStyle.alignSelf || style.alignItems;
+
+      if (item === null) {
+        itemStyle[crossSize] = (align === 'stretch') ? lineCrossSize : 0;
+      }
+
+      if (align === 'flex-start') {
+        itemStyle[crossStart] = crossBase;
+        itemStyle[crossEnd] = itemStyle[crossStart] + crossSign * itemStyle[crossSize];
+      }
+
+      if (align === 'flex-end') {
+        itemStyle[crossEnd] = crossBase; + crossSign * lineCrossSize
+        itemStyle[crossStart] = itemStyle[crossEnd] - crossSign * itemStyle[crossSize];
+      }
+      if (align === 'center') {
+        itemStyle[crossStart] = crossBase + crossSize * (lineCrossSize - itemStyle[crossSize]) / 2;
+        itemStyle[crossEnd] = itemStyle[crossStart] + crossSign * itemStyle[crossSize];
+      }
+      if (align === 'stretch') {
+        itemStyle[crossStart] = crossBase;
+        itemStyle[crossEnd] = crossBase + crossSign * itemStyle[crossSize];
+        //代码待补全；
+        itemStyle[crossSize] = crossSign * (itemStyle)
+      }
+
+
+    }
+
+    crossBase += crossSign * (lineCrossSize + step)
+  })
+
 }
 
 module.exports = layout;
