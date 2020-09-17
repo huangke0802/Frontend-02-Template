0aa3e47ca06f1d000b15c27e135a7d4ce1b01eee
huangke <huangke0802@163.com>
2020-09-17 23:32:28 +0800

    轮播组件开发

--- week12/jsx/main.js
+++ week12/jsx/main.js
@@ -1,81 +1,39 @@
-function createElement(type, attributes, ...children) {
-	let element;
-	if('string' === typeof type){
-		element = new ElementWrapper(type);
-	}else{
-		element = new type;
-	}
-	for(let name in attributes){
-		element.setAttribute(name, attributes[name]);
-	}
-	for(let child of children){
-		if('string' === typeof child){
-			child = new TextWrapper(child);
-		}
-		element.appendChild(child);
-	}
-  return element;
-}
+import { Component, createElement } from './framework'
 
-class ElementWrapper{
-	constructor(type){
-		this.root = document.createElement(type);
+class Carousel extends Component {
+	constructor() {
+		super();
+		this.attributes = Object.create(null);
 	}
 
 	setAttribute(name, value){
-		this.root.setAttribute(name, value);
+		this.attributes[name] = value;
 	}
 
-	appendChild(child){
-		child.mountTo(this.root);
-	}
-
-	mountTo(parent){
-		parent.appendChild(this.root);
-	}
-}
-
-class TextWrapper{
-	constructor(content){
-		this.root = document.createTextNode(content);
-	}
-
-	setAttribute(name, value){
-		this.root.setAttribute(name, value);
-	}
-
-	appendChild(child){
-		child.mountTo(this.root);
-	}
-
-	mountTo(parent){
-		parent.appendChild(this.root);
-	}
-}
-
-class Div {
-	constructor(){
+	render() {
 		this.root = document.createElement('div');
+		this.root.classList.add('carousel')
+		for(let record of this.attributes.src){
+			let child = document.createElement('div');
+			child.style.backgroundImage = `url(${record})`;
+			this.root.appendChild(child);
+		}
+		return this.root;
 	}
-	setAttribute(name, value){
-		this.root.setAttribute(name, value);
-	}
-
-	appendChild(child){
-		child.mountTo(this.root)
-	}
-
+	
 	mountTo(parent){
-		parent.appendChild(this.root);
+		parent.appendChild(this.render());
 	}
 }
 
-let a = <Div id = 'a'>
-		<span>a</span>
-		<span>b</span>
-		<span>c</span>
-		<span>d</span>
-	</Div>
+let d = [
+	'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1600363951966&di=2362c568bf1a3381d0680cef86fea819&imgtype=0&src=http%3A%2F%2Fdpic.tiankong.com%2F5d%2Fi0%2FQJ8166844632.jpg',
+	'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1600363951967&di=d426c6f293e8f7d658eaa35c38d3d131&imgtype=0&src=http%3A%2F%2Fdpic.tiankong.com%2Fpi%2Fr1%2FQJ6624335187.jpg',
+	'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1600363951966&di=1c1a6c5c2bbdcbf0782ce8f8142b9200&imgtype=0&src=http%3A%2F%2Fstatic.ettoday.net%2Fimages%2F929%2F929384.jpg',
+	'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1600363951965&di=2c4686b8365f9f420f7a703bbb9b1e55&imgtype=0&src=http%3A%2F%2Fwww.lanmaofangzi.com%2Fwp-content%2Fuploads%2F2015%2F06%2Fxiaorubai2.jpg'
+]
 
-	// document.body.appendChild(a)
-	a.mountTo(document.body)
+let a = <Carousel src={d} />
+
+// document.body.appendChild(a)
+a.mountTo(document.body)
