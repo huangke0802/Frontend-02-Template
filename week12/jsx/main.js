577a2112040843d104993d109bb0a30d5ece6868
huangke <huangke0802@163.com>
2020-09-19 00:06:40 +0800

    添加鼠标事件

--- week12/jsx/main.js
+++ week12/jsx/main.js
@@ -6,22 +6,68 @@ class Carousel extends Component {
 		this.attributes = Object.create(null);
 	}
 
-	setAttribute(name, value){
+	setAttribute(name, value) {
 		this.attributes[name] = value;
 	}
 
 	render() {
 		this.root = document.createElement('div');
 		this.root.classList.add('carousel')
-		for(let record of this.attributes.src){
+		for (let record of this.attributes.src) {
 			let child = document.createElement('div');
 			child.style.backgroundImage = `url(${record})`;
 			this.root.appendChild(child);
 		}
+
+		//自动滚动
+		/* let currentIndex = 0;
+		setInterval(() => {
+			let children = this.root.children;
+
+			let nextIndex = (currentIndex + 1) % children.length;
+
+			let current = children[currentIndex];
+			let next = children[nextIndex];
+
+			next.style.transition = "none";
+			next.style.transform = `translateX(${100 - nextIndex * 100}%)`;
+
+			setTimeout(() => {
+				next.style.transition = "";
+				current.style.transform = `translateX(${ -100 - nextIndex * 100 }%)`
+				next.style.transform = `translateX(${ - nextIndex * 100}%)`;
+
+				currentIndex = nextIndex
+			}, 16)
+			
+		}, 3000) */
+
+		//鼠标事件
+		this.root.addEventListener('mousedown', event => {
+			console.log('mousedown')
+
+			let move = event => {
+				console.log("mousemove")
+			}
+
+			let up = event => {
+				console.log('up')
+
+				document.removeEventListener('mousemove', move);
+				document.removeEventListener('mousemove', up);
+			}
+
+			document.addEventListener('mousemove', move)
+			document.addEventListener('mouseup', up)
+
+		})
+
+
+
 		return this.root;
 	}
-	
-	mountTo(parent){
+
+	mountTo(parent) {
 		parent.appendChild(this.render());
 	}
 }
@@ -33,7 +79,7 @@ let d = [
 	'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1600363951965&di=2c4686b8365f9f420f7a703bbb9b1e55&imgtype=0&src=http%3A%2F%2Fwww.lanmaofangzi.com%2Fwp-content%2Fuploads%2F2015%2F06%2Fxiaorubai2.jpg'
 ]
 
-let a = <Carousel src={d} />
+let a = <Carousel src={ d } />
 
 // document.body.appendChild(a)
 a.mountTo(document.body)
