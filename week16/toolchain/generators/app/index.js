51e360c55e55d7a1c0436c811361ea2bbfe3c841
huangke0802 <huangke0802@163.com>
2020-10-20 16:29:31 +0800

    搭建文集系统，包依赖系统

--- week16/toolchain/generators/app/index.js
+++ week16/toolchain/generators/app/index.js
@@ -4,11 +4,34 @@ module.exports = class extends Generator {
   constructor(args, opts) {
     super(args, opts);
   }
-  method1() {
-    this.log('method 1 just ran');
+  async step1() {
+
+    const answers = await this.prompt([{
+      type: "input",
+      name: "name",
+      message: "Your project name",
+      default: this.appname
+    }, ]);
+
+    const pkgJson = {
+      devDependencies: {
+        eslint: '^3.15.0'
+      },
+      dependencies: {
+        react: "^16.2.0"
+      }
+    }
+
+    this.fs.extendJSON(this.destinationPath('package.json'), pkgJson);
+
+    this.npmInstall();
+
+    this.fs.copyTpl(
+      this.templatePath('index.html'),
+      this.destinationPath('public/index.html'), {
+        title: answers.name
+      }
+    );
   }
 
-  method2() {
-    this.log('method 2 just ran');
-  }
 };
